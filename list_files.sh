2824c691e5e08835ca544df4307a9ae178c676ac
Yassinmohamed1 <134326002+Yassinmohamed1@users.noreply.github.com>
2024-12-27 03:20:51 +0200

    Add bash script to list files

--- list_files.sh
+++ list_files.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+ HEAD
 # This script lists all files and directories in the current directory
 ls -la
+
+# This script will list the contents of the current directory
+ls
+
+ 9b2ba84 (Add bash script to list files)
